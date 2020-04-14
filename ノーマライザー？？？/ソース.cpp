7274e88a02710bf59ddd41d40676ed2c5465a45b
Satori32 <Huga8989+githubf@gmail.com>
2020-04-14 19:47:05 +0900

    Add MapMethod.

--- ノーマライザー？？？/ソース.cpp
+++ ノーマライザー？？？/ソース.cpp
@@ -8,14 +8,26 @@ double DeNormalizer(double V, double Max) {
 }
 
 int main() {
-
-	std::size_t Max = 70;
-	double V = 0;
-	while (std::cin >> V) {
-		double T = Normalizer(V, Max);
-		std::cout <<V<<':'<< T << std::endl;
-		T = DeNormalizer(T, Max);
-		std::cout <<V<<':'<< T << std::endl;
+	{
+		std::size_t Max = 70;
+		double V = 0;
+		while (std::cin >> V) {
+			double T = Normalizer(V, Max);
+			std::cout << V << ':' << T << std::endl;
+			T = DeNormalizer(T, Max);
+			std::cout << V << ':' << T << std::endl;
+		}
 	}
+
+	std::size_t Max1 = 70;
+	std::size_t Max2 = 90;
+	double V = 30;
+
+	double T = Normalizer(V, Max1);
+	double R = DeNormalizer(T, Max2);
+
+	std::cout << V << ':' << T << ':' << R << std::endl;
+
+	return 0;
 
 }
